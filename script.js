5289e038ef03590f91997fd1b90a0a7b1514c434
Francisco Castillo <frankycstl@gmail.com>
2023-01-03 12:27:56 -0800

    save tab btn
    save tab btn that interacts with google api to access current tab URL

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // local variables
 let myLeads = []
+let listItems = []
 
 const storedLeads = JSON.parse(localStorage.getItem("myLeads"))
 const inputEl = document.getElementById("input-el")
 const saveBtn = document.getElementById("save-btn")
+const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
@@ -27,6 +29,18 @@ saveBtn.addEventListener("click", function () {
     }
 })
 
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query ({active: true, currentWindow: true}, function(tabs) {
+    // tabs variable will look something like this:
+    // const tabs = [
+        // {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+    // ]
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
+
 // clears myLeads array and local storage
 deleteBtn.addEventListener("dblclick", function () {
     localStorage.clear()
@@ -37,7 +51,7 @@ deleteBtn.addEventListener("dblclick", function () {
 // rends any new leads onto the screen
 // -------------------- My way --------------------
 function render(arr) {
-    let listItems = []
+    listItems = []
     for (let i = 0; i < arr.length; i++) {
         listItems += `
         <li>
